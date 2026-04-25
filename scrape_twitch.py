dac5b29ac7432ef8492f1bea84acb3339fd83d0b
Maximus Fernandez <maximusfernandez98@gmail.com>
2026-04-25 19:29:03 -0400

    tried using playwright stealth but figured it would be too much of a hassle -- discontinued

--- scrape_twitch.py
+++ scrape_twitch.py
@@ -10,6 +10,8 @@
 # JavaScript template render the table, then we read raw values directly
 # from the DOM.
 
+from playwright_stealth import stealth_sync
+
 from playwright.sync_api import sync_playwright
 import pandas as pd
 import time
